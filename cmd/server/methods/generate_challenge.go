18ae4ae923e46a2dd74a9b280c7abdac6b0c8533
David Goldstein <david.goldstein@deciphernow.com>
2019-03-01 18:03:13 -0500

    wrote tests for validate challenge request

--- cmd/server/methods/generate_challenge.go
+++ cmd/server/methods/generate_challenge.go
@@ -10,7 +10,18 @@ import (
 
 // get challenge token
 func (s *serverData) GenerateChallenge(ctx context.Context, request *pb.ChallengeRequest) (*pb.ChallengeResponse, error) {
+	// validate request
+	// is user in db?
+	// is user locked out?
+	// location is known || answer is in header?
+	// answer already in db?
+	// generate challenge
+	// add login to list
 
 	
 	return nil, errors.New("not implemented")
+}
+
+func ValidateChallengeRequest(request *pb.ChallengeRequest) error {
+	return errors.New("not implemented")
 }
