51a14263bb9c137fd63d578f6089b83487fd706b
David Goldstein <david.goldstein@deciphernow.com>
2019-02-28 20:37:17 -0500

    added tests for new server

--- cmd/server/methods/new_server_test.go
+++ cmd/server/methods/new_server_test.go
@@ -6,8 +6,15 @@ package methods
 
 import (
 	"testing"
+	"github.com/rs/zerolog"
+	"google.golang.org/grpc"
+	"os"
 )
 
 func TestCreateAndRegisterServer(t *testing.T) {
-	
+	logger := zerolog.New(os.Stderr).With().Timestamp().Logger().Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	zerolog.SetGlobalLevel(5)
+
+	server := grpc.NewServer()
+	CreateAndRegisterServer(logger, server)
 }
