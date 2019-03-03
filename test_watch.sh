04df8678381f24107dbb340d5a45fae8b63e8fc9
David Goldstein <david.goldstein@deciphernow.com>
2019-03-02 23:52:10 -0500

    refactored test_watch script

--- test_watch.sh
+++ test_watch.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 while true; do
 
-inotifywait -e modify,create,delete -r ./cmd && \
-clear
-# wait to build
-# sleep 5 
-## run unit and integration tests
-go test -v ./cmd/server/methods/ -covermode=count -coverprofile=./cmd/server/methods/coverage.out && \
-npm run testCI --prefix integration-tests/
+run_tests() {
+	go test -v ./cmd/server/methods/ -covermode=count -coverprofile=./cmd/server/methods/coverage.out
+	if [ $? -eq 0 ]; then
+		npm run testCI --prefix integration-tests/
+	fi
+}
 
+inotifywait -e modify,create,delete -r ./cmd && \
+	clear
+	run_tests
 done
